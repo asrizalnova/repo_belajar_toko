7606d58051e871575d7bbd95df440c4faf18e0cc
asrizal <asrizal7889gmail.com>
2020-08-08 11:44:51 +0700

    Migrasi Database db_toko

---
+++ database/migrations/2020_08_07_021538_create_pembayaran_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePembayaranTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembayaran', function (Blueprint $table) {
+            $table->bigIncrements('id_pembayaran');
+            $table->date('tanggal_bayar');
+            $table->integer('total_bayar');
+            $table->integer('id_orders');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembayaran');
+    }
+}
