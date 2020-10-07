716d5a5625781d96a7a2fa3ddc5de5f26f11b7d1
asrizal <asrizal7889gmail.com>
2020-10-07 16:54:57 +0700

    Menambah pembagian hak akses

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,6 +30,7 @@ class UserController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
+            'type' => 'required|integer'
         ]);
 
         if ($validator->fails()) {
@@ -40,6 +41,7 @@ class UserController extends Controller
             'name' => $request->get('name'),
             'email' => $request->get('email'),
             'password' => Hash::make($request->get('password')),
+            'type' => $request->get('type'),
         ]);
         $token = JWTAuth::fromUser($user);
 
--- routes/api.php
+++ routes/api.php
@@ -5,30 +5,50 @@ use Illuminate\Http\Request;
 Route::post('/register', 'UserController@register');
 Route::post('/login', 'UserController@login');
 
-Route::group(['middleware' => ['jwt.verify']], function ()
-{
+Route::group(['middleware' => ['jwt.verify']], function () {
+
+    Route::group(['middleware' => ['api.superadmin']], function ()
+    {
+        Route::delete('/Customers/{id}', 'CustomersController@destroy');
+        Route::delete('/Orders/{id}', 'OrdersController@destroy');
+        Route::delete('/Pembayaran/{id}', 'PembayaranController@destroy');
+        Route::delete('/Product/{id}', 'ProductController@destroy');
+    });
+
+    Route::group(['middleware' => ['api.admin']], function ()
+    {
+        Route::post('/Customers', 'CustomersController@store');
+        Route::put('/Customers/{id}', 'CustomersController@update');
+
+        Route::post('/Orders', 'OrdersController@store');
+        Route::put('/Orders/{id}', 'OrdersController@update');
+
+        Route::post('/Pembayaran', 'PembayaranController@store');
+        Route::put('/Pembayaran/{id}', 'PembayaranController@update');
+
+        Route::post('/Product', 'ProductController@store');
+        Route::put('/Product/{id}', 'ProductController@update');
+    });
+
+
     Route::get('/Customers', 'CustomersController@show');
     Route::get('/Customers/{id}', 'CustomersController@detail');
-    Route::post('/Customers', 'CustomersController@store');
-    Route::put('/Customers/{id}', 'CustomersController@update');
-    Route::delete('/Customers/{id}', 'CustomersController@destroy');
+   
+   
     
     Route::get('/Orders', 'OrdersController@show');
     Route::get('/Orders/{id}', 'OrdersController@detail');
-    Route::post('/Orders', 'OrdersController@store');
-    Route::put('/Orders/{id}', 'OrdersController@update');
-    Route::delete('/Orders/{id}', 'OrdersController@destroy');
+   
+   
     
     Route::get('/Pembayaran', 'PembayaranController@show');
     Route::get('/Pembayaran/{id}', 'PembayaranController@detail');
-    Route::post('/Pembayaran', 'PembayaranController@store');
-    Route::put('/Pembayaran/{id}', 'PembayaranController@update');
-    Route::delete('/Pembayaran/{id}', 'PembayaranController@destroy');
+   
+   
     
     Route::get('/Product', 'ProductController@show');
     Route::get('/Product/{id}', 'ProductController@detail');
-    Route::post('/Product', 'ProductController@store');
-    Route::put('/Product/{id}', 'ProductController@update');
-    Route::delete('/Product/{id}', 'ProductController@destroy');
-
-});
+    
+    
+}
+);
